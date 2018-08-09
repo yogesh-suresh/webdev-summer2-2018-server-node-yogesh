a0dae6904e6b35eff54988107743a2d1af79a2fc
Yogesh Suresh <suresh.y@husky.neu.edu>
2018-08-08 20:38:50 -0400

    Mongo URI

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-mongoose.connect(MONGODB_URI);
+mongoose.connect('mongodb://heroku_8qsrv6tp:k0orb86lsi0bf80gn7mjtd3vbb@ds039175.mlab.com:39175/heroku_8qsrv6tp');
 
 
 var app = express()
